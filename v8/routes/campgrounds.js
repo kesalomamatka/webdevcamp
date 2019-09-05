fe29548c0a1d18b41175a6a803d71b2139d0d034
Zhihao <paradisetan798@gmail.com>
2019-09-05 18:00:24 +0300

    v9 done

--- v8/routes/campgrounds.js
+++ v8/routes/campgrounds.js
@@ -20,11 +20,7 @@ router.post("/",isLoggedIn,  function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var author ={
-        id:req.user._id,
-        username: req.user.username
-    }
-    var newCampground = {name: name, image: image, description: desc, author: author}
+    var newCampground = {name: name, image: image, description: desc}
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
